0942562ec1fefad056d26ca332db0aa8dfffa6d1
l3002 <tanmaymathpal4545@gmail.com>
2024-01-26 05:14:22 +0530

    Initial commit
    Signed-off-by: l3002 <tanmaymathpal4545@gmail.com>

---
+++ src/main/java/com/bgm/webapp/BGMWebApp.java
@@ -0,0 +1,13 @@
+package com.bgm.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BGMWebApp {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BGMWebApp.class, args);
+  }
+
+}
---
+++ src/test/java/com/bgm/webapp/BGMWebAppTest.java
@@ -0,0 +1,13 @@
+package com.bgm.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BGMWebAppTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
