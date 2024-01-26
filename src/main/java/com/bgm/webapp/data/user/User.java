4108b863302db5348e9119e41146db4662247130
l3002 <tanmaymathpal4545@gmail.com>
2024-01-26 11:40:06 +0530

    fix: Constructor related issues with User.java
    Signed-off-by: l3002 <tanmaymathpal4545@gmail.com>

--- src/main/java/com/bgm/webapp/data/user/User.java
+++ src/main/java/com/bgm/webapp/data/user/User.java
@@ -11,13 +11,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 @Data
 @Builder
-@RequiredArgsConstructor
+@AllArgsConstructor
 @Entity
 public class User implements UserDetails {
 
