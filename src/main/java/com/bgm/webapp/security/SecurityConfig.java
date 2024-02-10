d07c124f16d6c37402fde34d5c26efbec1dd3e39
l3002 <tanmaymathpal4545@gmail.com>
2024-02-10 09:37:42 +0530

    feat: implemented User Authentication through db
    Signed-off-by: l3002 <tanmaymathpal4545@gmail.com>

--- src/main/java/com/bgm/webapp/security/SecurityConfig.java
+++ src/main/java/com/bgm/webapp/security/SecurityConfig.java
@@ -1,33 +1,63 @@
 package com.bgm.webapp.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.bgm.webapp.data.user.User;
+import com.bgm.webapp.data.user.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
 
-  // private final UserRepository userRepository;
+  private final UserRepository userRepository;
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
         .csrf(csrf -> csrf.disable())
         .authorizeHttpRequests(
-            authorizeHttpRequestsCustomizer -> authorizeHttpRequestsCustomizer
-                .requestMatchers("/").permitAll()
-                .anyRequest().authenticated());
+            authorizeHttpRequestsCustomizer -> authorizeHttpRequestsCustomizer.requestMatchers("/").permitAll()
+                .anyRequest().authenticated())
+        .authenticationProvider(daoAuthenticationProvider())
+        .formLogin(formLoginCostomizer -> formLoginCostomizer.permitAll());
     return http.build();
   }
 
-  /*
-   * @Bean
-   *
-   * public UserDetailsService userDetailsService(){
-   *
-   * return (username) -> userRepository.
-   * }
-   *
-   */
+  @Bean
+  public UserDetailsService userDetailsService() {
+    return (username) -> {
+      UserDetails user = userRepository.findByUserName(username).orElseThrow();
+      return user;
+    };
+  }
+
+  @Bean
+  public DaoAuthenticationProvider daoAuthenticationProvider() {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    authProvider.setUserDetailsService(userDetailsService());
+    return authProvider;
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+  }
+
 }
