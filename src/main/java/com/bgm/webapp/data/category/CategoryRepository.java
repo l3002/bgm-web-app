cdf8da0a29cf38033d7b2fa0858380834af4304a
l3002 <tanmaymathpal4545@gmail.com>
2024-02-24 16:02:08 +0530

    fix: added Repository annoation to category and product repositories
    Signed-off-by: l3002 <tanmaymathpal4545@gmail.com>

--- src/main/java/com/bgm/webapp/data/category/CategoryRepository.java
+++ src/main/java/com/bgm/webapp/data/category/CategoryRepository.java
@@ -1,10 +1,12 @@
 package com.bgm.webapp.data.category;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * CategoryRepository
  */
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 }
--- src/main/java/com/bgm/webapp/data/product/ProductRepository.java
+++ src/main/java/com/bgm/webapp/data/product/ProductRepository.java
@@ -1,10 +1,12 @@
 package com.bgm.webapp.data.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * ProductRepository
  */
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
