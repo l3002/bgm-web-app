2912b104522e07f50ef906ffca5c07e4d29d4580
l3002 <tanmaymathpal4545@gmail.com>
2024-02-24 15:23:53 +0530

    feat: added JPA Repository for Product Entity
    Signed-off-by: l3002 <tanmaymathpal4545@gmail.com>

---
+++ src/main/java/com/bgm/webapp/data/product/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.bgm.webapp.data.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * ProductRepository
+ */
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
