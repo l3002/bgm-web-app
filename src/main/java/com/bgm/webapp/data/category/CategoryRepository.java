6065b93f20557972175e4b3cb543374f764b7d70
l3002 <tanmaymathpal4545@gmail.com>
2024-02-24 15:23:16 +0530

    feat: added JPA Repository for Category Entity
    Signed-off-by: l3002 <tanmaymathpal4545@gmail.com>

---
+++ src/main/java/com/bgm/webapp/data/category/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.bgm.webapp.data.category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * CategoryRepository
+ */
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
