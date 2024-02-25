7ce2142546a4cc513496f5f635c855ed437e7b5c
l3002 <tanmaymathpal4545@gmail.com>
2024-02-25 21:25:52 +0530

    fix: added ManyToOne relationship for Product and Category
    Signed-off-by: l3002 <tanmaymathpal4545@gmail.com>

--- src/main/java/com/bgm/webapp/data/product/Product.java
+++ src/main/java/com/bgm/webapp/data/product/Product.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -30,6 +31,7 @@ public class Product {
   private String productName;
   private String description;
   private Map<String, String> details;
+  @ManyToOne
   private Category category;
 
 }
