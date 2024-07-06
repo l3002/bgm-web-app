b2e6397d2095ff958de673670039db6026f79c59
l3002 <tanmaymathpal4545@gmail.com>
2024-07-07 04:34:53 +0530

    fix: updated Product Class
    Signed-off-by: l3002 <tanmaymathpal4545@gmail.com>

--- src/main/java/com/bgm/webapp/data/product/Product.java
+++ src/main/java/com/bgm/webapp/data/product/Product.java
@@ -1,19 +1,9 @@
 package com.bgm.webapp.data.product;
 
-import java.util.Map;
-
-import com.bgm.webapp.data.category.Category;
-
-import jakarta.persistence.CollectionTable;
-import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.MapKeyColumn;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -35,12 +25,4 @@ public class Product {
   private Long productID;
   private String productName;
   private String description;
-  @ElementCollection
-  @CollectionTable(name = "property-value-mapping", joinColumns = { @JoinColumn(name = "id") })
-  @MapKeyColumn(name = "property")
-  @Column(name = "value")
-  private Map<String, String> details;
-  @ManyToOne
-  private Category category;
-
 }
